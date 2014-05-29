880f21e749cdd8b3f4cc8ca9891e8febea78e14a
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2014-05-29 22:27:54 +0930

    kernel_update: add ubuntu kernel update

--- kernel_update.sh
+++ kernel_update.sh
@@ -16,11 +16,14 @@ BOOT_SCR_FEDORA20_XU="http://builder.mdrjr.net/tools/boot.scr_fedora20_xu.tar"
 FIRMWARE_URL="http://builder.mdrjr.net/tools/firmware.tar.xz"
 
 # Kernel builds
-export K_PKG_URL="http://builder.mdrjr.net/kernel-3.8/$KERNEL_RELEASE"
-export XU_K_PKG_URL="http://builder.mdrjr.net/kernel-3.4/$KERNEL_RELEASE"
+export K_PKG_URL="http://builder.mdrjr.net/kernel-3.8/00-LATEST"
+export XU_K_PKG_URL="http://builder.mdrjr.net/kernel-3.4/00-LATEST"
 
 
 kernel_update() {
+	
+	get_board
+	
 	while true; do
 		KO=$(whiptail --backtitle "Hardkernel ODROID Utility v$_REV" --menu "Kernel Update/Configuration" 0 0 0 \
 		"1" "Update Kernel" \
@@ -53,22 +56,111 @@ kernel_update() {
 
 }
 
+do_kernel_download() {
+	rm -fr $KTMP
+	mkdir -p $KTMP
+	cd $KTMP
+	
+	if [ "$BOARD" = "odroidxu" ]; then
+		dlf_fast $XU_K_PKG_URL/$BOARD.tar.xz "Downloading ODROID-XU Kernel. Please wait." $KTMP/$BOARD.tar.xz
+	else
+		dlf_fast $K_PKG_URL/$BOARD.tar.xz "Downloading $BOARD Kernel. Please Wait." $KTMP/$BOARD.tar.xz
+	fi
+}
+
 do_kernel_update() {
-	# STUB
+	do_kernel_download
+	
+	case "$DISTRO" in 
+		"ubuntu")
+			do_ubuntu_kernel_update ;;
+		"debian")
+			do_debian_kernel_update ;;
+		*) msgbox "KERNEL-UPDATE: Distro not found. Shouldn't be here. Please report on the forums" ;;
+	esac
+
+	return 0
 }
 
 do_firmware_update() {
-	# STUB
+	echo
 }
 
 do_bootscript_update() {
-	# STUB
+	echo
 }
 
 do_udev_update() {
-	# STUB
+	echo 
 }
 
 do_bootloader_update() {
-	# STUB
+	echo 
+}
+
+do_ubuntu_kernel_update() { 
+	cd $KTMP
+	
+	echo "*** Installing new kernel. Please way. A backup and log will be saved on /root"
+	export klog=/root/kernel_update-log-$DATE.txt
+	
+	tar -zcf /root/kernel-backup-$DATE.tar.xz /lib/modules /media/boot &>> $klog
+	xz -d $BOARD.tar.xz &>> $klog
+	tar xf $BOARD.tar &>> $klog
+	
+	rm -fr /media/boot/zImage* /media/boot/uImage* /media/boot/uInitrd* /lib/modules/3.8.13* /lib/modules/3.4* &>> $klog
+	
+	cp -aRP boot/zImage /media/boot/zImage &>> $klog
+	cp -aRP lib/modules/* /lib/modules &>> $klog
+	
+	cat /etc/initramfs-tools/initramfs.conf | sed s/"MODULES=most"/"MODULES=dep"/g > /tmp/a.conf
+    mv /tmp/a.conf /etc/initramfs-tools/initramfs.conf
+
+    export K_VERSION=`ls $KTMP/boot/config-* | sed s/"-"/" "/g | awk '{printf $2}'`
+    cp $KTMP/boot/config-* /boot
+    update-initramfs -c -k $K_VERSION &>> $klog
+    mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n "uInitrd $K_VERSION" -d /boot/initrd.img-$K_VERSION /boot/uInitrd-$K_VERSION &>> $klog
+    cp /boot/uInitrd-$K_VERSION /media/boot/uInitrd 
+    
+    cp /lib/firmware/s5p-mfc/* /lib/firmware
+    
+    if [ "$BOARD" = "odroidxu" ]; then
+		axel -o $KTMP/hwc.tar -n 2 -q http://builder.mdrjr.net/tools/xubuntu_hwcomposer.tar &>> $klog
+		(cd /usr && tar xf $KTMP/hwc.tar) &>> $klog
+	fi
+
+	echo "exit 0" > /etc/initramfs/post-update.d/flash-kernel
+	chmod +x /etc/initramfs/post-update.d/flash-kernel
+	
+	if [ "$BOARD" != "odroidxu" ]; then
+		update_hwclock
+	fi
+
+	if [ "$BOARD" = "odroidx" ] || [ "$BOARD" = "odroidx2" ]; then
+		cp $KTMP/boot/zImage.lcdkit /media/boot
+	fi
+	
+	msgbox "KERNEL-UPDATE: Done. Your kernel should be updated now.
+	Check /root for the backup and log files.
+	BACKUP: /root/kernel-backup-$DATE.tar.gz
+	LOG: $klog"
+	
+	rm -fr $KTMP
+	
+	return 0
+	
+}
+
+
+update_hwclock() {
+        mv /sbin/hwclock /sbin/hwclock.orig
+        axel -o /sbin/hwclock -q http://builder.mdrjr.net/tools/hwclock &>> $klog
+        chmod 0755 /sbin/hwclock
+}
+
+install_headers() { 
+        rm -rf /usr/src/linux-* 
+        mv $KTMP/usr/src/linux* /usr/src
+        rm -fr /lib/modules/$K_VERSION/build
+        ln -s /usr/src/linux-$K_VERSION /lib/modules/$K_VERSION/build
 }
