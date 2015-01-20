365593896b755bf8c51ab947ec46588f1e116d3a
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-01-20 02:31:52 -0200

    xbmc: add instructions for C1

--- xbmc_setup.sh
+++ xbmc_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 xbmc_setup() {
+	get_board
+	if [ "$BOARD" = "odroidc" ]; then
+		msgbox "Use apt-get install kodi   or   apt-get update && apt-get dist-upgrade"
+		return
+	fi
 	msgbox "Not supported yet"
 }
