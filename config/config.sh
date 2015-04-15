3a13b5b9305efb1fb82d62e81f980479a7fc84ed
api-walker <elias.froehner@gmail.com>
2015-04-15 21:06:04 +0200

    Updated config.sh

--- config/config.sh
+++ config/config.sh
@@ -5,21 +5,37 @@ REBOOT=0
 DATE=`date +%Y.%m.%d-%H.%M`
 
 # Load the scripts
-source $_B/extras.sh
-source $_B/fs_resize.sh
-source $_B/hdmi.sh
-source $_B/kernel_update.sh
-source $_B/xbmc_setup.sh
-source $_B/xorg_config.sh
-source $_B/armsoc.sh
-source $_B/change_hostname.sh
-source $_B/lirc_setup.sh
-source $_B/odroidc_hdmipass.sh
-source $_B/pulseaudio_control.sh
+
+# Config
+source $_B/config/extras.sh
+
+# Multimedia
+source $_B/multimedia/hdmi.sh
+source $_B/multimedia/xbmc_setup.sh
+source $_B/multimedia/lirc_setup.sh
+source $_B/multimedia/odroidc_hdmipass.sh
+source $_B/multimedia/pulseaudio_control.sh
+
+# Administration
+source $_B/administration/armsoc.sh
+source $_B/administration/fs_resize.sh
+source $_B/administration/kernel_update.sh
+source $_B/administration/xorg_config.sh
+source $_B/administration/change_hostname.sh
+source $_B/administration/change_password.sh
+source $_B/administration/change_locale.sh
+source $_B/administration/change_timezone.sh
+source $_B/administration/configure_keyboard.sh
+
+# Menu
+source $_B/menu/internationalization_menu.sh
+source $_B/menu/administration_menu.sh
+source $_B/menu/multimedia_menu.sh
+source $_B/menu/desktop_environment_menu.sh
 
 
 # Get the board here
 get_board
 
 # mainmenu should be the last one :)
-source $_B/mainmenu.sh
+source $_B/menu/mainmenu.sh
