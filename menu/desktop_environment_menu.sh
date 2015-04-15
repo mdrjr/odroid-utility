cf06e27a55126a73cab31477c683b4ef4f28c929
api-walker <elias.froehner@gmail.com>
2015-04-15 20:59:21 +0200

    Add new menus

---
+++ menu/desktop_environment_menu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+desktop_environment_menu() {
+	msgbox "Changing your desktop environment can be dangerous. Be careful!"
+
+	while true; do
+		DEO=$(whiptail --backtitle "Hardkernel ODROID Utility v$_REV" --menu "Desktop Environment/Configuration" 0 0 1 --cancel-button "Back" --ok-button "Select"\
+		"1" "Install KDE" \
+		"2" "Install XFCE" \
+		"3" "Install LXDE" \
+		"4" "Fully remove KDE" \
+		"5" "Fully remove XFCE" \
+		"6" "Fully remove LXDE" \
+		3>&1 1>&2 2>&3)
+	
+		DER=$?
+	
+		if [ $DER -eq 1 ]; then
+			return 0
+		else
+			case "$DEO" in 
+				"1") install_kde ;;
+				"2") install_xfce ;;
+				"3") install_lxde ;;
+				"4") remove_kde ;;
+				"5") remove_xfce ;;
+				"6") remove_lxde ;;
+				*) msgbox "Desktop Environment: Error. You shouldn't be here. Value $DEO please report this on the forums" ;;
+			esac
+		fi
+		msgbox "Logout and change the desktop environment in login manager."	
+
+	done
+}
+
+install_kde() {
+	apt-get update -y
+	apt-get install -y kubuntu-desktop
+}
+
+install_xfce() {
+	apt-get update -y
+	apt-get install -y xubuntu-desktop
+}
+
+install_lxde() {
+	apt-get update -y
+	apt-get install -y lubuntu-desktop
+}
+
+remove_kde() {
+	apt-get autoremove -y --purge kubuntu-desktop
+}
+
+remove_xfce() {
+	apt-get autoremove -y --purge xubuntu-desktop
+}
+
+remove_lxde() {
+	apt-get autoremove -y --purge lxde
+	apt-get autoremove -y --purge lubuntu-desktop
+}
---
+++ menu/internationalization_menu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+internationalization_menu() {
+	
+	while true; do
+		IO=$(whiptail --backtitle "Hardkernel ODROID Utility v$_REV" --menu "Internationalization/Configuration" 0 0 0 --cancel-button "Back" --ok-button "Select"\
+		"1" "Change locale" \
+		"2" "Change timezone" \
+		"3" "Change keyboard layout" \
+		3>&1 1>&2 2>&3)
+	
+		IR=$?
+	
+		if [ $IR -eq 1 ]; then
+			return 0
+		else
+			case "$IO" in 
+				"1") change_locale ;;
+				"2") change_timezone ;;
+				"3") configure_keyboard ;;
+				*) msgbox "Internationalization: Error. You shouldn't be here. Value $IO please report this on the forums" ;;
+			esac
+		fi
+	
+	done
+}
