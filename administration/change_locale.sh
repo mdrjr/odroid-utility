d8f6cb66aecf250754467bf7dc36042af8f746ba
api-walker <elias.froehner@gmail.com>
2015-04-16 17:04:40 +0200

    Update locale config to cmdline

--- administration/change_locale.sh
+++ administration/change_locale.sh
@@ -2,11 +2,27 @@
 
 change_locale() {
  
- locales=$(locale -a)
-  
- selected_locale=$(whiptail --inputbox "$locales" 20 54 3>&1 1>&2 2>&3)
+ #bad implementation:
+ #locales=$(locale -a)  
+ #selected_locale=$(whiptail --inputbox "$locales" 20 54 3>&1 1>&2 2>&3)
  
+ #using cmdline
+ echo "Please select one of the following locales:"
+
+ #sleep 1 second
+ sleep 1
+
+ #show all locales
+ locale -a
+
+ echo "Locale:"
+ #read locale
+ read selected_locale
+
+ #update
  update-locale LANG=$selected_locale
  
  dpkg-reconfigure locales
+ 
+ msgbox "Please restart!"
 }
