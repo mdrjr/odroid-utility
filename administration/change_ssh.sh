3b07051194454a275f9a73a4246f8c859bcc9794
Elias Fröhner <elias.froehner@gmail.com>
2015-04-19 22:12:56 +0200

    Add support for ssh configuration

---
+++ administration/change_ssh.sh
@@ -0,0 +1,21 @@
+#copied from https://github.com/asb/raspi-config/blob/master/raspi-config#L327
+
+do_ssh() {
+  if [ -e /var/log/regen_ssh_keys.log ] && ! grep -q "^finished" /var/log/regen_ssh_keys.log; then
+    whiptail --msgbox "Initial ssh key generation still running. Please wait and try again." 20 60 2
+    return 1
+  fi
+  whiptail --yesno "Would you like the SSH server enabled or disabled?" 20 60 2 \
+    --yes-button Enable --no-button Disable
+  RET=$?
+  if [ $RET -eq 0 ]; then
+    update-rc.d ssh enable &&
+    invoke-rc.d ssh start &&
+    whiptail --msgbox "SSH server enabled" 20 60 1
+  elif [ $RET -eq 1 ]; then
+    update-rc.d ssh disable &&
+    whiptail --msgbox "SSH server disabled" 20 60 1
+  else
+    return $RET
+  fi
+}
