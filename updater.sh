d6deec39b19ff2ddbc31ac0065beadd4f4662635
api-walker <elias.froehner@gmail.com>
2015-04-20 22:17:22 +0200

    Redo updater.sh

--- updater.sh
+++ updater.sh
@@ -2,23 +2,31 @@
 _B="/usr/local/bin"
 
 if [ `whoami` != "root" ]; then
-			sudo $0 $*
-			exit $?
+			echo "You must run the app as root."
+			echo "sudo $0"
+			exit 0
 fi
-
 if ! whiptail --yesno "This updater will remove the prevoius version and update to the newest! Do you agree?" 0 0; then
 	echo "User canceled update."
 	exit 0
 else
-	old_files=('armsoc.sh' 'change_hostname.sh' 'config.sh' 'config.sh' 'extras.sh' 'files.txt')
-
-	for i in "$(old_files[@])"
-	do
-		echo i
-	done
-
-	#rm $_B/
-
+	#remove old files
+	rm $_B/armsoc.sh
+	rm $_B/change_hostname.sh
+	rm $_B/config.sh
+	rm $_B/extras.sh
+	rm $_B/files.txt
+	rm $_B/fs_resize.sh
+	rm $_B/hdmi.sh
+	rm $_B/kernel_update.sh
+	rm $_B/mainmenu.sh
+	rm $_B/odroid-utility.sh
+	rm $_B/xbmc_setup.sh
+	rm $_B/xorg_config.sh
+	rm $_B/lirc_setup.sh
+	rm $_B/odroidc_hdmipass.sh
+	rm $_B/pulseaudio_control.sh
+	
 	#copy new content
 	cp -r ./administration $_B
 	cp -r ./config $_B
